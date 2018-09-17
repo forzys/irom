e194b7293f2dc31894358f6dc17aef58e9fa33e6
xujiang <xujiang156@qq.com>
2018-09-17 17:09:15 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <editor @output="output" />
+
+        <div id="con">
+
+        </div>
+    </div>
+</template>
+<script>
+import editor from '@/components/editor/'
+    export default{
+        name:'Home',
+        components:{
+            editor,
+        },
+        methods:{
+            output(con){
+                document.getElementById('con').innerHTML=con
+            }
+        }
+    }
+</script>
+ <style lang="">
+     
+ </style>
